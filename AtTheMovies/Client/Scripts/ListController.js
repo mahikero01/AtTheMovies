c2ab0d070388adbea429f37496b5aed464af594c
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2016-09-06 04:12:48 +0800

    up

---
+++ AtTheMovies/Client/Scripts/ListController.js
@@ -0,0 +1,27 @@
+﻿(function (app) {
+    var ListController = function ($scope, movieService) {
+        movieService
+            .getAll()
+            .success(function (data) {
+                 $scope.movies = data;
+            });
+
+        $scope.delete = function (movie) {
+            movieService.delete(movie)
+                .success(function () {
+                    removeMovieById(movie.Id);
+                });
+        };
+
+        var removeMovieById = function (id) {
+            for (var i = 0; i < $scope.movies.length; i++) {
+                if($scope.movies[i].Id == id){
+                    $scope.movies.splice(i, 1);
+                    break;
+                }
+            }
+        };
+    };
+
+    app.controller("ListController", ListController);
+})(angular.module("atTheMovies"));
