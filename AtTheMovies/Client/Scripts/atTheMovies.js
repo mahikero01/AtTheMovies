19995dd292080f265b545b7c26fa1e65cc186ed6
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2016-09-06 04:11:50 +0800

    up

--- AtTheMovies/Client/Scripts/atTheMovies.js
+++ AtTheMovies/Client/Scripts/atTheMovies.js
@@ -1,1 +1,16 @@
-﻿
+﻿(function () {
+    var app = angular.module("atTheMovies", ["ngRoute"]);
+
+    var config = function ($routeProvider) {
+        $routeProvider
+            .when("/list",
+                { templateUrl: "/client/views/list.html" })
+            .when("/details/:id",
+                { templateUrl: "/client/views/details.html" })
+            .otherwise(
+                { redirectTo: "/list" });
+    };
+
+    app.config(config);
+    app.constant("movieApiUrl", "/api/movies/");
+})();
